c31e24a5ba60c753f4ef860733dd6b14ec5a3479
Chris Gibson <chris.david.gibson@gmail.com>
2012-10-25 08:45:51 -0500

    Adding ruby files to demonstrate single responsibility principle

---
+++ SingleRepsonsibility/multiple_responsibility.rb
@@ -0,0 +1,19 @@
+
+class Modem
+  def dial number
+    #some code that establishes a connection
+  end
+
+  def hang_up
+    #some code that hangs up connection
+  end
+
+  def send datum
+    #some code that sends some datum
+  end
+
+  def receive
+    #some code that receives data
+  end
+end
+
---
+++ SingleRepsonsibility/single_reponsibility.rb
@@ -0,0 +1,19 @@
+class ModemConnection
+  def dial number
+    #some code that establishes a connection
+  end
+
+  def hang_up
+    #some code that hangs up connection
+  end
+end
+
+class ModemCommunication
+  def send datum
+    #some code that sends some datum
+  end
+
+  def receive
+    #some code that receives data
+  end
+end
